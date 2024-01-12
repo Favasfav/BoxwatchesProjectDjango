0e22e337e5242e26ce538379f89c4b3d05ab155a
Favasfav <favas.fav@gmail.com>
2024-01-12 17:48:51 +0530

    for render hosting db connect 3

--- boxwatches/urls.py
+++ boxwatches/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=staticfiles_urlpatterns()
 handler404 = 'products.views.custom_404_page'
